88376622e6446db00d2a5764c99f0eaa93e319b4
DeadBryam <Bryamu.u@gmail.com>
2024-04-10 00:50:53 -0600

    feat: create clock card component

---
+++ components/clock-card.tsx
@@ -0,0 +1,64 @@
+import { RiTimeFill } from "@remixicon/react";
+import { useCallback, useState } from "react";
+
+import styles from "@/styles/components/clock-card.module.css";
+
+import { classNames } from "../utils";
+import { AnalogClock } from "./analog-clock";
+import { Card } from "./card";
+
+interface ClockCardProps {
+  time: Date;
+  title: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+function ClockCard(props: ClockCardProps): JSX.Element {
+  const { time, title, className, onClick } = props;
+
+  const [isNight, setIsNight] = useState<boolean>(false);
+
+  const updateNight = useCallback((date: Date) => {
+    const hour = date.getHours();
+    setIsNight(hour < 6 || hour > 18);
+  }, []);
+
+  const handleClick = useCallback(() => {
+    if (onClick) {
+      onClick();
+    }
+  }, [onClick]);
+
+  return (
+    <Card
+      onClick={handleClick}
+      className={classNames(
+        className,
+        styles.card,
+        isNight ? styles.darkCard : styles.lightCard,
+      )}
+    >
+      <RiTimeFill
+        size={18}
+        className={classNames(
+          styles.icon,
+          isNight ? styles.darkIcon : styles.lightIcon,
+        )}
+      />
+      <p
+        id="clock-card-title"
+        data-testid="title"
+        className={classNames(
+          styles.title,
+          isNight ? styles.darkTitle : styles.lightTitle,
+        )}
+      >
+        {title}
+      </p>
+      <AnalogClock time={time} onChange={updateNight} />
+    </Card>
+  );
+}
+
+export { ClockCard };
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export * from "./analog-clock";
 export * from "./card";
+export * from "./clock-card";
 export * from "./custom-button";
 export * from "./suggestions-input";
