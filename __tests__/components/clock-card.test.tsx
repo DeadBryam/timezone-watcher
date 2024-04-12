2de3b4f6caa331dc8c4aeef463c7e32e5041ad91
DeadBryam <Bryamu.u@gmail.com>
2024-04-12 16:46:03 -0600

    test: update clock card tests

--- __tests__/components/clock-card.test.tsx
+++ __tests__/components/clock-card.test.tsx
@@ -1,10 +1,18 @@
 import { render } from "@testing-library/react";
+import { LocalTimeZone } from "@types";
 
 import { ClockCard } from "@/components/index";
 
 describe("Test ClockCard component", () => {
   test("renders ClockCard component", () => {
-    const { getByTestId } = render(<ClockCard offset={7200000} title="Test" />);
+    const timezone: LocalTimeZone = {
+      offset: 7200000,
+      region: "Test",
+      diff: 2,
+      timezone: "CEST",
+    };
+
+    const { getByTestId } = render(<ClockCard timeZone={timezone} />);
 
     const card = getByTestId("card");
     expect(card).toBeInTheDocument();
