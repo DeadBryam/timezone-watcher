6f988dd5b61790d240675200274e357a99080d88
DeadBryam <Bryamu.u@gmail.com>
2024-04-12 00:42:17 -0600

    refactor(app): add layout and toasts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+
+import { Layout } from "@/components/index";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </Layout>
+  );
 }
